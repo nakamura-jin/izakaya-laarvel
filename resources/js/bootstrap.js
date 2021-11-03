fd7a7e8f9e93b225f125bc0d05e7fe9d810fddb4
中村　迅 <shu.kyu0224@icloud.com>
2021-11-04 00:41:12 +0900

    bootstrap.php session.php

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,7 +9,6 @@ window._ = require('lodash');
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.headers.common['X-CSRF-TOKEN'] = window.Laravel.csrfToken;
 
 axios.defaults.withCredentials = true;
 
