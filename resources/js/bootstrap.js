1371d73a9bd39074093b2d032740fe762027e94f
中村　迅 <shu.kyu0224@icloud.com>
2021-11-08 18:48:16 +0900

    bootstrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,6 +9,9 @@ window._ = require('lodash');
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.withCredentials = true;
+window.axios.defaults.baseURL = "https://demo-izakaya-backend.herokuapp.com";
+window.axios.defaults.headers.common['X-CSRF-TOKEN'] = $('meta[name="csrf-token"]').attr('content');
 
 
 /**
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', [AuthenticatedSessionController::class, 'create']);
